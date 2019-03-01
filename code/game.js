1d0d5241788e191fc2e1bb53be474dbe961b6ea1
owen-flynn <owenflynn34@gmail.com>
2019-03-01 13:59:51 +0000

    added update grid function

--- code/game.js
+++ code/game.js
@@ -30,10 +30,36 @@ var count_live_neighbours = (grid,row,col,rows,cols) => {
     return live_neighbours;
 };
 
+var set_square = (grid,row,col,live_neighbours) => {
+    let value = grid[row][col];
+
+    if((value == 1 && (live_neighbours == 2 || live_neighbours == 3)) ||
+        (value == 0 && (live_neighbours == 3))) {
+            return 1;
+    }
+
+    return 0;
+};
+
+var update_grid = (grid,rows,cols) => {
+    let new_grid = Array(rows).fill().map(() => Array(cols).fill(0));
+    var row;
+    var col;
+
+    for(row = 0; row < rows; row++) {
+        for(col = 0; col < cols; col++) {
+            let live_neighbours = count_live_neighbours(grid,row,col,rows,cols);
+            new_grid[row][col] = set_square(grid,row,col,live_neighbours);
+        }
+    }
+
+    return new_grid;
+};
+
 let rows = 4;
 let cols = 4;
 
-grid = Array(rows).fill().map(() => Array(cols).fill(0));
+let grid = Array(rows).fill().map(() => Array(cols).fill(0));
 
 grid[0][0] = 1;
 grid[0][1] = 1;
@@ -43,3 +69,5 @@ grid[1][2] = 1;
 grid[2][0] = 1;
 grid[2][1] = 1;
 grid[2][2] = 1;
+
+updated_grid = update_grid(grid,rows,cols);
